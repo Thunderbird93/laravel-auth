f2846e44860809a64630bc2636ae540dcf724f8d
Thunderbird93 <antircristian@gmail.com>
2023-01-17 16:30:42 +0100

    end

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,12 +2,6 @@
 
 @section('content')
 <div class="container">
-    <@extends('layouts.app')
-
-    @section('content')
-    <div class="container">
-        <p> Nome: {{ Auth::user()->name}} eMail: {{Auth::user()->email}}</p>
-    </div>
-    @endsection
+    <p> Nome: {{ Auth::user()->name}} eMail: {{Auth::user()->email}}</p>
 </div>
 @endsection
